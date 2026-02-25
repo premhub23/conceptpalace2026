1b1fe90e10764817862b88eb397be55a5b828267
premhub23 <134101218+premhub23@users.noreply.github.com>
2026-02-25 12:16:48 -0400

    Update databaseconnect.php

--- includes/databaseconnect.php
+++ includes/databaseconnect.php
@@ -1,13 +1,6 @@
-
 <?php
-$host = getenv('DB_HOST');
-$user = getenv('DB_USER'); 
-$password = getenv('DB_PASS');
-$database = getenv('DB_NAME');
-$port = getenv('DB_PORT') ?: 5432;
-
-$dsn = "pgsql:host=$host;port=$port;dbname=$database";
-$conn = new PDO($dsn, $user, $password);
+$database_url = getenv('DATABASE_URL');
+$conn = new PDO($database_url);
 ?>
 
 
