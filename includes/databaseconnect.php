9a8f201a47e237d1dbb2f8a7629ae93912dd2a07
premhub23 <134101218+premhub23@users.noreply.github.com>
2026-02-24 22:27:08 -0400

    Update databaseconnect.php

--- includes/databaseconnect.php
+++ includes/databaseconnect.php
@@ -5,12 +5,12 @@ $username = $_ENV['DB_USER'];
 $password = $_ENV['DB_PASS'];
 $database = $_ENV['DB_NAME'];
 	
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $database);
+$conn = new mysqli($servername, $username, $password, $database);
 
 // Check connection
-if (!$conn) {
-  die("Connection failed: " . mysqli_connect_error());
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
 
 ?>
+
