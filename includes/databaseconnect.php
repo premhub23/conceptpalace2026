324e9e6baf393fed3e8901ad030fa0827bc6762c
premhub23 <134101218+premhub23@users.noreply.github.com>
2026-02-25 11:28:57 -0400

    Update databaseconnect.php

--- includes/databaseconnect.php
+++ includes/databaseconnect.php
@@ -1,20 +1,14 @@
-<?php
 
+<?php
 $host = getenv('DB_HOST');
-$user = getenv('DB_USER');
+$user = getenv('DB_USER'); 
 $password = getenv('DB_PASS');
 $database = getenv('DB_NAME');
 $port = getenv('DB_PORT') ?: 5432;
-	
-$conn = new mysqli($host, $user, $password, $database, $port);
 
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
+$dsn = "pgsql:host=$host;port=$port;dbname=$database";
+$conn = new PDO($dsn, $user, $password);
 ?>
-
 
 
 
